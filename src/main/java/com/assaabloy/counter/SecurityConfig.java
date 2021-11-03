13907b3f38998638da5bd738807a3ff0de562681
gadiyavinay <gadiyavinay@yahoo.co.in>
2021-11-03 10:38:32 +0100

    Updated web security config

--- src/main/java/com/assaabloy/counter/SecurityConfig.java
+++ src/main/java/com/assaabloy/counter/SecurityConfig.java
@@ -1,15 +1,19 @@
-//package com.assaabloy.counter;
-//
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//public class SecurityConfig extends WebSecurityConfigurerAdapter{
-//
-//	@Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//            .anyRequest()
-//            .permitAll()
-//            .and().csrf().disable();
-//    }
-//}
+package com.assaabloy.counter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest()
+            .permitAll()
+            .and().csrf().disable();
+    }
+}
